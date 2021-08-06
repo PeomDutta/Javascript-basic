1182c4ee85e678aa06b39d228fc3de99c84497e1
itspeomd <peomd04@gmail.com>
2021-08-07 03:17:29 +0600

    max-array

---
+++ max-array.js
@@ -0,0 +1,16 @@
+// Find the maximum value from an Array
+
+function maxArray(numbers) {
+
+    var max = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
+
+var arrayNumbers = [10, 21, 24, 5, 19];
+var result = maxArray(arrayNumbers);
+console.log("Maximum value is: ", result);
