74964f09948b3f69e014899087838dd4c554e5a9
itspeomd <peomd04@gmail.com>
2021-08-07 04:05:54 +0600

    fibo-push

--- fibonacci.js
+++ fibonacci.js
@@ -6,15 +6,19 @@ function fibonacci(numbers) {
     var fibo1 = 0;
     var fibo2 = 1;
     var fibo = fibo1 + fibo2;
+    var fibonacciOutput = [0, 1]
 
     for (let i = 2; i < numbers; i++) {
         console.log(fibo);
+        fibonacciOutput.push(fibo);
         fibo1 = fibo2;
         fibo2 = fibo;
         fibo = fibo1 + fibo2;
     }
+    return fibonacciOutput;
 
 }
 
 var num = 8;
-fibonacci(num);
+var result = fibonacci(num);
+console.log(result);
